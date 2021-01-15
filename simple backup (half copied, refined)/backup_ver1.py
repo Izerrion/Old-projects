b7bbb71901efe51eb443199cefe17250fe07f34f
Izerrion <71769024+Izerrion@users.noreply.github.com>
2021-01-15 12:36:57 +0300

    Update backup_ver1.py

--- simple backup (half copied, refined)/backup_ver1.py
+++ simple backup (half copied, refined)/backup_ver1.py
@@ -1,28 +1,19 @@
 import os
 import time
 
-# 1. The files and directories to be backed up are
-# specified in a list.
-# example on our OS:
 source = ['/root/Documents/Notes']
 
-# 2. The backup must be stored in a
-# main backup directory
-# example on our OS:
+
 target_dir = '/root/HDD/backup'
 
-# 3. The files are backed up into a zip file
-# 4. Name of the zip file archive is the current date and time
+
 target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
 
-#Create the target directory if it is not present
-#if not os.path.exists(target_dir):
-#    os.mkdir(target_dir)
 
-# 5. We use the zip command to put the files in a zip archive
+
 zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
 
-#Run the backup
+
 print('Zip command is:')
 print(zip_command)
 print('Running:')
